2627dadb2303892b47686bb085f37b0060a9e66b
diyooor <diyorsattarov7@gmail.com>
2023-08-28 00:55:13 -0500

    Added logger, updated server with product webpage, styled homepage dark theme, included product images...

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./logger'); // Adjust the path as needed
 
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('static'));
+
+// Middleware to log requests
+app.use((req, res, next) => {
+    logger.info(`Request made to: ${req.method} ${req.url}`);
+    next();
+});
 
 // Homepage route
 app.get('/', (req, res) => {
@@ -14,14 +23,19 @@ app.get('/', (req, res) => {
 
 // About page route
 app.get('/about', (req, res) => {
-    res.render('about'); // Create an "about.ejs" view template
+    res.render('about');
 });
 
 // Contact page route
 app.get('/contact', (req, res) => {
-    res.render('contact'); // Create a "contact.ejs" view template
+    res.render('contact');
+});
+
+// Contact page route
+app.get('/products', (req, res) => {
+    res.render('products');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    logger.info(`Server is running on http://localhost:${port}`);
 });
