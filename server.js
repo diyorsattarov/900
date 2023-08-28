32889feb9e7ebcd85175dd72119893cf8fea62e8
diyooor <diyorsattarov7@gmail.com>
2023-08-28 01:30:52 -0500

    added create-payment-intent

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser'); // Add this line
 const logger = require('./logger'); // Adjust the path as needed
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Add this line
 
 const app = express();
 const port = 3000;
@@ -9,6 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('static'));
+app.use(bodyParser.json()); // Add this line to parse JSON request bodies
 
 // Middleware to log requests
 app.use((req, res, next) => {
@@ -31,10 +34,35 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-// Contact page route
+// Products page route
 app.get('/products', (req, res) => {
     res.render('products');
 });
+
+// Payment handling route
+app.post('/create-payment-intent', async (req, res) => {
+    try {
+        const { items } = req.body; // Assuming you're sending product details in the request body
+        // Calculate total amount based on items
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: calculateTotalAmount(items), // Replace with your logic
+            currency: 'usd', // Replace with your desired currency
+        });
+
+        res.status(200).json({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+        console.error('Error creating payment intent:', error.message);
+        res.status(500).json({ error: 'An error occurred while processing your request.' });
+    }
+});
+
+// Helper function to calculate total amount based on items
+function calculateTotalAmount(items) {
+    // Implement your logic to calculate the total amount based on items
+    // Example: return items.reduce((total, item) => total + item.price, 0);
+	return 3000;
+}
 
 app.listen(port, () => {
     logger.info(`Server is running on http://localhost:${port}`);
